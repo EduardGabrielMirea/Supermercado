2a8205a1cb5ac72dc537fdd7bde57b168730bca8
EduardGabrielMirea <eduardmirea00@gmail.com>
2024-06-25 16:14:19 +0200

    Modificación de la clase Main, con el objetivo de introducir un menu que se use mediante consola.

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,36 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    private static int currentScreen = 0;
 
+    public static void main(String[] args) {
+        int opcion;
+        while (true){
+            imprimirMenu();
+            opcion = obtenerOpcion();
+            if (opcion == 0) break;
+            if (currentScreen == 0){
+                switch (opcion){
+                    case 1:
+                        crearDetergente();
+                        break;
+                    case 2:
+                        crearCereal();
+                        break;
+                    case 3:
+                        crearVino();
+                        break;
+                    case 4:
+                        calcularCalorias();
+                        break;
+                }
+            }
+        }
+
+        /*
         Detergente detergenteAriel = new Detergente("Fairy", 5.95f);
         detergenteAriel.setVolumen(1.5f);
         detergenteAriel.setTipoEnvase("Botella");
@@ -41,6 +67,60 @@ public class Main {
             totalCalorias += producto.getCalorias();
         }
         System.out.println("\nSuma de calorías de los productos alimenticios: " + totalCalorias);
+    */
+    }
+
+    private static void imprimirMenu(){
+        System.out.println("------------------------------------------");
+        System.out.println(" 0- Salir | 1- Crea un detergente | 2- Crea un cereal | 3- Crea un vino | 4- Calcula calorias");
+        System.out.println("------------------------------------------");
+
+    }
+
+    private static int obtenerOpcion(){
+        Scanner sc = new Scanner(System.in);
+        int opcion = -1;
+        try {
+            opcion = Integer.parseInt(sc.next());
+            if (currentScreen == 0 && opcion < 1 || opcion > 6) {
+            }
+        }catch (NumberFormatException nfe){
+            System.out.println("Opcion incorrecta");
+        }
+        return opcion;
+    }
+
+    private static void crearDetergente(){
+        System.out.println("Intoduce la marca, el precio del detergente, el volumen, el tipo de envase y el descuento. (Fairy 10 2 Botella 0.2)");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Marca: ");
+        String marca = sc.next();
+        System.out.println("Precio: ");
+        String precio = sc.next();
+        System.out.println("Volumen: ");
+        String volumen = sc.next();
+        System.out.println("Tipo de envase: ");
+        String tipoEnvase = sc.next();
+        System.out.println("Descuento: ");
+        String descuento = sc.next();
+
+        Detergente detergente = new Detergente(marca, Float.parseFloat(precio));
+        detergente.setVolumen(Float.parseFloat(volumen));
+        detergente.setTipoEnvase(tipoEnvase);
+        detergente.setDescuento(Float.parseFloat(descuento));
+
+        System.out.println("Se ha creado correctamente el detergente: "+ detergente);
+    }
+    private static void crearVino(){
+
+    }
+
+    private static void crearCereal(){
+
+    }
+
+    private static void calcularCalorias(){
+
     }
 }
 
